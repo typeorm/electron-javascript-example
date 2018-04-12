b43a2ba21594999824eaebc6df4b37392b9a55bf
Umed Khudoiberdiev <zarrhost@gmail.com>
2018-04-12 19:25:28 +0500

    initial commit

---
+++ src/entity/Post.js
@@ -0,0 +1,18 @@
+var EntitySchema = require("typeorm").EntitySchema;
+
+module.exports = new EntitySchema({
+    name: "Post",
+    columns: {
+        id: {
+            type: Number,
+            primary: true,
+            generated: true
+        },
+        title: {
+            type: String
+        },
+        text: {
+            type: String
+        }
+    }
+});
